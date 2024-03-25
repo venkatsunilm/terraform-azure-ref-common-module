f24cf62e25aff3fc2ae68886b1bdbfba1da10df2
venkatsunilm <venkatsunilminchala@gmail.com>
2024-03-26 01:15:52 +0200

    :bug: confi: fix config issues and updated to latest versions

--- main.tf
+++ main.tf
@@ -1,5 +1,18 @@
+terraform {
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
+  }
+
+  # backend "azurerm" {}
+}
+
 provider "azurerm" {
-  version = "~> 1.39"
+  # skip_provider_registration = true # This is only required when the User, Service Principal, or Identity running Terraform lacks the permissions to register Azure Resource Providers.
+  features {}
 }
 
 resource "azurerm_resource_group" "common" {
